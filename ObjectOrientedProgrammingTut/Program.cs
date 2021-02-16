a2d7cc25195d4862500cd2c11fa5243727c258b4
Dustin Fox <dustinfox1996@gmail.com>
2021-02-16 16:10:38 -0500

    Add project files.

---
+++ ObjectOrientedProgrammingTut/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObjectOrientedProgrammingTut {
+    class Program {
+        static void Main(string[] args) {
+            var sqr1 = new Sqr(7);
+            Console.WriteLine($" SQR: Perimeter is {sqr1.Perimeter() }");
+            Console.WriteLine($" SQR: Area is {sqr1.Area() }");
+
+            var rect1 = new Rect(5, 7);
+            // up above now works and it less work becasue of the work we did on the Rect class with the base attached to our methods
+            //rect1.Side1 = 3;
+            //rect1.Side2 = 4;
+            //rect1.Side3 = 5;
+            //rect1.Side4 = 6;
+            Console.WriteLine($"Perimeter is {rect1.Perimeter() }");
+            Console.WriteLine($"Area is {rect1.Area() }");
+            //Console.WriteLine($"About is {rect.About});
+        }
+    }
+}
